2304581905d82e2ff5abfee494d64f437f03181e
aaryanchaubey <aaryanchaubey693@gmail.com>
2024-06-23 23:55:51 +0530

    second

--- myapp/src/server.js
+++ myapp/src/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-const uri = "mongodb+srv://Janhavi:savita@cluster0.xopqd2q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://username:password@cluster0.xopqd2q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const client = new MongoClient(uri, {
 	serverApi: {
--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-const uri = "mongodb+srv://Janhavi:savita@cluster0.xopqd2q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://username:password@cluster0.xopqd2q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 mongoose.connect(uri);
 const Patient = require('./models/Patient');
 const Doctor = require('./models/Doctor');
